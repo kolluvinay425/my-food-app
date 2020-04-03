023f0ccedec4e39c69d26d5513832740bf551465
vinay <kolluvinay425@gmail.com>
2020-04-03 23:03:22 +0200

    mywebsite app, first commit

---
+++ food/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Item
+from django.template import loader
+from .forms import ItemForm
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+
+# function views.
+def index(request):
+    item_list = Item.objects.all()
+    
+    context ={
+        'item_list':item_list,
+     }
+    return render(request,'food/index.html',context)
+# class based view
+
+class IndexClassView(ListView):
+    model = Item;
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'      
+
+class FoodDetail(DetailView):
+    model = Item
+    template_name = 'food/detail.html'
+
+def detail(request,item_id):
+    items = Item.objects.get(pk=item_id)
+    
+    return render(request,'food/detail.html',{'items':items})
+    
+def create_item(request):
+     form = ItemForm(request.POST or None)
+
+     if form.is_valid():
+         form.save()
+         return redirect('food:index')
+     return render(request,'food/item_form.html',{'form':form}) 
+#this is class based view of create_item
+
+class CreateItem(CreateView):
+    model = Item;
+    fields = ['item_name','item_desc','item_price','item_image']
+    template_name='food/item_form.html'
+
+    def form_valid(self,form):
+        form.instance.user_name = self.request.user
+
+        return super().form_valid(form)
+
+
+
+
+def update_item(request,id):
+    item = Item.objects.get(id=id)   
+    form = ItemForm(request.POST or None, instance=item)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')    
+
+    return render(request,'food/item_form.html',{'form':form,'item':item})
+
+def delete_item(request,id):
+    item = Item.objects.get(id=id)
+ 
+    if request.method == 'POST':
+         item.delete()
+         return redirect('food:index')
+ 
+    return render(request,'food/item-delete.html',{'item':item})
+
+
+
+     
